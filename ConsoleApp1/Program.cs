50e9b7a10ebff8d346bcc6c9a6fc2a6f4b7230a1
visualstudio <visualstudio@230-PC34>
2019-02-07 14:49:40 -0500

    commit at 2.49

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace _2019WCSD3354WEEK05
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = new testques2();
+            a.Play();
+        }
+    }
+
+    class testques2
+    {
+        public int myfavvari = 0;
+        public void Play()
+        {
+            //write a for loop
+
+            for (; my();)
+            {
+                if (myfavvari <= 10)
+                    Console.WriteLine("you are fool");
+                break;
+            }
+            Console.WriteLine("shit for loop again");
+        }
+        public bool my()
+        {
+            myfavvari++;
+            return true;
         }
     }
 }
